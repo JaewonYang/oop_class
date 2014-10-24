8efc9b03f9a97ed9f5f5a5406cb493d07300f20d
Jaewon <skyboyddd@gmail.com>
2014-10-24 11:42:32 +0900

    Second Commit on Oct.24 Before noon.
    Change add sum method and example

--- IdeaProjects/Exam/src/test/java/JaewonTest.java
+++ IdeaProjects/Exam/src/test/java/JaewonTest.java
@@ -8,8 +8,11 @@ public class JaewonTest {
     public static void main(String[] args){
         Calculator c= new Calculator();
 
-        double result = c.multiple(10, 50);
-        assertEquals(500, result, 0);
+        double multiple_result = c.multiple(10, 50);
+        assertEquals(500, multiple_result, 0);
+
+        int sum_result = c.sum(5,10);
+        assertEquals(15, sum_result, 0);
     }
 }
 
@@ -17,4 +20,7 @@ public class JaewonTest {
     double multiple(double num1, double num2){
        return num1*num2;
     }
+    int sum(int num1, int num2){
+         return num1+num2;
+     }
  }
