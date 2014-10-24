baa4c6fb613fd184e6bc754b3eebae3ea04a1924
Jaewon <skyboyddd@gmail.com>
2014-10-24 11:20:39 +0900

    First Commit on Oct.24 Before noon.

---
+++ IdeaProjects/Exam/src/test/java/JaewonTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by ce416-17 on 2014. 10. 24..
+ */
+
+public class JaewonTest {
+    public static void main(String[] args){
+        Calculator c= new Calculator();
+
+        double result = c.sum(10, 50);
+        assertEquals(60, result, 0);
+
+
+    }
+
+
+}
+
+ class Calculator{
+    double sum(double num1, double num2){
+       return num1+num2;
+    }
+ }
